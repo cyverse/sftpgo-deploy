c087a0f54cc0c62f4dcb27ac62505a48ed31ceda
Illyoung Choi <iychoi@email.arizona.edu>
2023-04-11 16:51:58 -0700

    Upgrade sftpgo to 2.4.x

--- sftpgo/scripts/prep-config-json.sh
+++ sftpgo/scripts/prep-config-json.sh
@@ -4,10 +4,18 @@
 #
 # It requires the following environment variables to be defined
 #
+# WEBDAV_PORT                     The TCP port for the webdav service
 # SFTP_PORT                       The TCP port for the sftp service
 # SFTPGO_ADMIN_UI_PORT            The TCP port for web admin service
 # SFTPGO_VAULT                    The local path for storing user data and log
 # SFTPGO_HOME_PATH                The local path for storing user home data
+# IRODS_PROXY_USER                The proxy user for iRODS to provide authentication of users
+# IRODS_PROXY_PASSWORD            The password of the proxy user for iRODS
+# IRODS_HOST                      The iRODS hostname
+# IRODS_PORT                      The iRODS port number
+# IRODS_ZONE                      The iRODS zone name
+# IRODS_SHARED                    The iRODS shared directory name (e.g., public / shared)
+
 
 main()
 {
@@ -31,10 +39,17 @@ escape()
 expand_tmpl()
 {
   cat <<EOF | sed --file - /tmp/sftpgo.json.template
+s/\$WEBDAV_PORT/$(escape $WEBDAV_PORT)/g
 s/\$SFTP_PORT/$(escape $SFTP_PORT)/g
 s/\$SFTPGO_ADMIN_UI_PORT/$(escape $SFTPGO_ADMIN_UI_PORT)/g
 s/\$SFTPGO_VAULT/$(escape $SFTPGO_VAULT)/g
 s/\$SFTPGO_HOME_PATH/$(escape $SFTPGO_HOME_PATH)/g
+s/\$IRODS_PROXY_USER/$(escape $IRODS_PROXY_USER)/g
+s/\$IRODS_PROXY_PASSWORD/$(escape $IRODS_PROXY_PASSWORD)/g
+s/\$IRODS_HOST/$(escape $IRODS_HOST)/g
+s/\$IRODS_PORT/$(escape $IRODS_PORT)/g
+s/\$IRODS_ZONE/$(escape $IRODS_ZONE)/g
+s/\$IRODS_SHARED/$(escape $IRODS_SHARED)/g
 EOF
 }
 
